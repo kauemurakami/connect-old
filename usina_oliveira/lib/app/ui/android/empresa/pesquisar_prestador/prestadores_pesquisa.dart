6ed66dcc686075eaa1bf0c655c02a5ced03309f8
Kauê Murakami <kauetmurakami@gmail.com>
2020-07-02 15:38:09 -0300

    adicionando resultado de pesquisa do prestador

--- usina_oliveira/lib/app/ui/android/empresa/pesquisar_prestador/prestadores_pesquisa.dart
+++ usina_oliveira/lib/app/ui/android/empresa/pesquisar_prestador/prestadores_pesquisa.dart
@@ -6,6 +6,7 @@ import 'package:usina_oliveira/app/data/repository/empresa_repository.dart';
 import 'package:http/http.dart' as http;
 import 'package:usina_oliveira/app/data/repository/prestador_repository.dart';
 import 'package:usina_oliveira/app/ui/android/widgets/icon_button_back_widget.dart';
+import 'package:usina_oliveira/app/ui/theme/app_text_theme.dart';
 
 class PrestadoresPesquisaPage extends StatelessWidget {
 //repository injection
@@ -16,35 +17,72 @@ class PrestadoresPesquisaPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            IconButtonBackWidget(),
-            Expanded(
-              child: GetX<PesquisarPrestadoresController>(
-                  init: PesquisarPrestadoresController(
-                      repository: this.prestadorRepository),
-                  builder: (_) {
-                    return ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (context, index) {
-                        return Card(
-                          child: Column(
-                            children: <Widget>[
-                              Container(
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(image: Image.network(''))
-                                ),
-                              )
-                            ],
-                          ),
-                        );
-                      },
-                      itemCount: 5,
-                    );
-                  }),
-            )
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              IconButtonBackWidget(),
+              GetX<PesquisarPrestadoresController>(
+                init: PesquisarPrestadoresController(
+                    repository: this.prestadorRepository),
+                builder: (_) {
+                  return Container(
+                      height: MediaQuery.of(context).size.height,
+                      width: MediaQuery.of(context).size.width,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: Obx(() => Card(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Image.network(
+                                        'https://source.unsplash.com/random/300x300',
+                                        width: 300,
+                                        height: 300,
+                                      ),
+                                      SizedBox(height: 20),
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Row(
+                                          children: <Widget>[
+                                            Container(
+                                              decoration: BoxDecoration(
+                                                  borderRadius:
+                                                      BorderRadius.circular(10),
+                                                  border: Border.all(
+                                                      width: 1,
+                                                      color: Colors.lightGreen)),
+                                              child: Padding(
+                                                  padding: EdgeInsets.all(4),
+                                                  child: Text(
+                                                    'Categoria',
+                                                    style: destaqueText,
+                                                  )),
+                                            ),
+                                            Padding(
+                                                  padding: EdgeInsets.only(left: 16),
+                                                  child: Text(
+                                                    'Cidade',
+                                                    style: destaqueText,
+                                                  )),
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                )),
+                          );
+                        },
+                        itemCount: _.prestadores.length,
+                      ));
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
